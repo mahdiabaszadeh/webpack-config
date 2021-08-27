b4e39afbcb7fe5e9c568d82cbc204b6bfd80a76b
mahdiabaszadeh <mahdiabaszadeh1995@gmail.com>
2021-08-27 16:28:35 +0430

    image support to webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,24 @@
+/** @format */
+
 import "../styles/index.scss";
 
-import React from 'react'
+import React from "react";
 import Recipies from "./Recipies";
-function App() {
-    return (
-        <>
-            <section className="hero"></section>
-            <main>
-                <section>
-                    <h1>
-                        I'm with React
-                    </h1>
-                </section>
-            </main>
 
-            <Recipies />
-        </>
-    )
+import photo from "../images/pic.jpg";
+function App() {
+  return (
+    <>
+      <section className="hero"></section>
+      <main>
+        <section>
+          <h1>I'm with React</h1>
+        </section>
+        <img src={photo} alt="photo" width="250" />
+        <Recipies />
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
